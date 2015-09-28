f92eeeb3fa5c417c913a92f34446545feea38c29
themaxtech <maxtechapps@gmail.com>
2015-09-28 12:55:49 +0530

    Af
    Af

--- www/js/index.js
+++ www/js/index.js
@@ -344,9 +344,9 @@ $(document).on('pagecontainershow', function (e, ui) {
                         "    background: #e9e9e9;" +
                         "}" +
                         "</style>" +
-                        "<div id='year_calendar' data-role='page' data-theme='a'><div data-role='header' data-position='fixed' class='toolbar'>" + 
-                        " <h2>Message</h2><a class='back' href='#arunhome'>Back</a></div>" +
-                        "<div data-role='content'>" + 
+                        "<div id='year_calendar' data-role='page' data-theme='a'><div data-role='header' data-position='fixed'>" + 
+                        "<a href='#arunhome' class='ui-btn ui-icon-carat-l ui-btn-icon-notext ui-corner-all'></a>" + 
+                        "<h2>Message</h2></div><div data-role='content'>" + 
                         "</div><div data-role='footer' data-position='fixed'>"+
                         "<p>  &copy;  2015 www.themaxtech.com</p></div></div>"; 
 
