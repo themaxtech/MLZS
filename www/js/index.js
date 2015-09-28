fd3bb31a02ec5c0142174bc908c047f3c692b344
themaxtech <maxtechapps@gmail.com>
2015-09-28 13:15:12 +0530

    A1
    A1

--- www/js/index.js
+++ www/js/index.js
@@ -64,6 +64,10 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+
+        if (parseFloat(window.device.version) === 7.0) {
+          document.body.style.marginTop = "20px";
+        }
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
